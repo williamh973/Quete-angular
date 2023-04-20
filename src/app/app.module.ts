c66894db3db3921db075e9749118958af4cdf92a
williamh973 <126678879+williamh973@users.noreply.github.com>
2023-04-20 09:23:48 +0200

    firstCommit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignUpComponent,
+    UserProfileComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule
